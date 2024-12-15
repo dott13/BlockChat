615df3c8b02683e5337eea549cfb83e4a75cbfb1
dott13 <supreme.notfound@gmail.com>
2024-12-15 23:33:36 +0200

    made essentials and the project structure for the frontend

---
+++ frontend/src/components/UserMainPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserMainPage: React.FC = () => {
+    return (
+        <div className="p-4">
+            <h2 className="text-xl font-bold">Welcome to your dashboard!</h2>
+            <p>This is the main page for regular users.</p>
+        </div>
+    )
+}
+
+export default UserMainPage;
